4eb970ce36ab929e12c3e103f12441eb4e9c9604
Lucas Holmquist <lholmqui@redhat.com>
2024-04-12 12:24:38 -0400

    squash: it would help to spell correctly

--- catalog-service/src/Services/inventory.service.ts
+++ catalog-service/src/Services/inventory.service.ts
@@ -3,12 +3,14 @@ import axios from 'axios';
 export class InventoryService {
 
     static async getInventoryItem(itemId: string) {
-        const inventoryServiceHost = process.env.INVERNTORY_SERVICE_HOST || 'localhost'
+        const inventoryServiceHost = process.env.INVENTORY_SERVICE_HOST || 'localhost'
         try {
             // Needs baseURL
             //TODO: needs to be configurabe
             //TODO: lets switch to fetch and remove a dep
-            const response = await axios.get(`http://${inventoryServiceHost}:8080/api/v1/inventory/${itemId}`);
+            const baseURL = `http://${inventoryServiceHost}:8080/api/v1/inventory/${itemId}`;
+            console.log(baseURL);
+            const response = await axios.get(baseURL);
             const inventoryItem = response.data;
             return inventoryItem;
         } catch (error) {
