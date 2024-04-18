630381122cd11a1daf95c5dd0c2daabe85c2c704
Lucas Holmquist <lholmqui@redhat.com>
2024-04-18 13:20:36 -0400

    squash: front-end

--- front-end/app/services/catalog.js
+++ front-end/app/services/catalog.js
@@ -4,9 +4,9 @@ angular.module("app")
 
 .factory('catalog', ['$http', '$q', 'COOLSTORE_CONFIG', 'Auth', '$location', function($http, $q, COOLSTORE_CONFIG, $auth, $location) {
 	var factory = {}, products, baseUrl;
-    baseUrl = 'https://catalog-node-dev.apps.ci-ln-wfw1mxt-76ef8.aws-2.ci.openshift.org/api/v1/products';
+   //  baseUrl = 'https://catalog-node-dev.apps.ci-ln-wfw1mxt-76ef8.aws-2.ci.openshift.org/api/v1/products';
 //     baseUrl = $location.protocol() + '://localhost:7072/api/v1/products';
-    // baseUrl = $location.protocol() + '://catalog-' + COOLSTORE_CONFIG.OCP_NAMESPACE + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1") + '/api/products';
+    baseUrl = $location.protocol() + '://catalog-' + COOLSTORE_CONFIG.OCP_NAMESPACE + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1") + '/api/products';
 
     factory.getProducts = function() {
 		var deferred = $q.defer();
--- front-end/app/services/orders.js
+++ front-end/app/services/orders.js
@@ -5,8 +5,8 @@ angular.module("app")
 .factory('orders', ['$http', '$q', 'COOLSTORE_CONFIG', 'Auth', '$location', function($http, $q, COOLSTORE_CONFIG, $auth, $location) {
     var factory = {}, orders,baseUrl;
 
-	baseUrl = 'https://orders-node-dev.apps.ci-ln-wfw1mxt-76ef8.aws-2.ci.openshift.org/api/orders';
-	// baseUrl = $location.protocol() + '://order-' + COOLSTORE_CONFIG.OCP_NAMESPACE + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1") + '/api/orders';
+	// baseUrl = 'https://orders-node-dev.apps.ci-ln-wfw1mxt-76ef8.aws-2.ci.openshift.org/api/orders';
+	baseUrl = $location.protocol() + '://orders-' + COOLSTORE_CONFIG.OCP_NAMESPACE + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1") + '/api/orders';
 	//baseUrl = $location.protocol() + '://localhost:3000/api/orders';
     factory.getOrders = function() {
 
